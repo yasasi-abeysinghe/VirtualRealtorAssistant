440cfa4c914c4aefe705e814d817b2968f3a5d4c
yasasi <yaabeysinghe@gmail.com>
2022-12-19 00:46:49 -0500

    Updated the main.py to invoke voice2text

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import api_calls
 import visualize_results
+import voice2text
 
 
 if __name__ == '__main__':
-    results = api_calls.get_results("Norfolk Virginia", ["home", "condo"], 1, 0, 300000)
+    location, price, room, date = voice2text.run_voice2text()
+    # location, price, room, date = "Norflok, Virginia", "300000", "2", "July"
+    results = api_calls.get_results(location, ["home", "condo"], room, 0,
+                                    int(price.replace(",", "") if "," in price else price))
     visualize_results.generate_output(results)
